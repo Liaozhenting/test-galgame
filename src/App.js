d25e6d8801cbc26edf7215c45f630570780fefc1
Liaozhenting <15399901665@163.com>
2019-01-03 02:39:37 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './app.css';
+import './index.css';
+import menu_background from './images/menu.jpg';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App' id='webvn'>
+        <div className='center'>
+          <div id='menu' className='fill' style={{ display: 'block', opacity: 1 ,background: `url(${menu_background}) no-repeat`}} >
+            <ul  >
+              <li className='start'>开始</li>
+              <li className='load'>读取</li>
+              <li className='cg'>画廊</li>
+              <li className='music'>音乐</li>
+              <li className='setting'>设置</li>
+            </ul>
+          </div>
+        </div>
+
       </div>
     );
   }
